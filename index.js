ba3bdd6e75dd04a63d8661ca72e5444e5ff9b139
github username <github email address>
2019-08-25 21:53:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(list, name){
+    return list.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(list, name) {
+    let length = name.length;
+    return list.filter(function (driverName) {
+        return driverName.slice(0, length) === name;
+    })
+}
+
+function matchName(list, name) {
+    return list.filter(function(driverName) {
+        return driverName.name.toLowerCase() === name.toLowerCase();
+    })
+}
