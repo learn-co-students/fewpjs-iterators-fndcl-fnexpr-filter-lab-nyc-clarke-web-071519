ac606c020576176bfd35c57cbc36d9a073491fdb
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-08-26 12:01:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(driverarr, str){
+    return driverarr.filter(driver => {
+        return driver.toUpperCase() === str.toUpperCase()
+    })
+}
+
+// findMatching(['hello', 'goodbye'], 'hello')
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(x => {
+        return x.charAt() === string.charAt();
+    })
+}
+
+function matchName(drivers, str){
+    return drivers.filter(driver => {
+        return driver.name.toUpperCase() === str.toUpperCase()
+    })
+
+}
+
